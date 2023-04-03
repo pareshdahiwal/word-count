d1b441f41a9915f922f5a1b36525456868c28673
pareshdahiwal <pareshdahiwal@gmail.com>
2023-04-03 20:50:14 +0100

    minor changes in printing messages

--- src/main/java/com/d/Main.java
+++ src/main/java/com/d/Main.java
@@ -8,18 +8,19 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        System.out.println("Inside main()...");
         if (args.length == 0 || args[0] == null) {
             System.out.println("Arguments missing. Expected fileName along with the path as first argument... e.g. java -jar \"c:\\myfile.txt\"");
             System.exit(0);
         }
-        System.out.println("You have passed file: " + args[0]);
 
         File file = new File(args[0]);
         if (!file.exists() || !file.isFile()) {
             System.out.println("Error: File not found. File: " + file.getAbsolutePath());
             System.exit(0);
         }
+
+        System.out.println("Below are all the words with their frequencies in file: " + args[0]);
+        System.out.println("---------------------------------------------------------------------");
 
         Map<String, Long> frequencyByWords = new WordFrequency().findInFiles(file.toPath());
         frequencyByWords.entrySet().forEach(System.out::println);
